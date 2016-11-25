8707575d48e95ec2213a74149f8570e876f67f85
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-25 21:52:15 +0100

    Class Addess, methods, instance variables

--- src/Training/Address.java
+++ src/Training/Address.java
@@ -1,19 +1,23 @@
 package Training;
 
-/**
- * Created by Михаил on 25.11.2016.
- */
 public class Address {
+    private int houseNum;
+    private String streetName;
+    private int apartNum;
+    private String city;
+    private String state;
+    private int postCode;
 
-    public Address (int num, String str, int apnum, String ct, String st, int posc) {
+
+    public Address(int num, String str, int apnum, String ct, String st, int posc) {
 
     }
 
-    public Address (int num, String str, String ct, String st, int posc) {
+    public Address(int num, String str, String ct, String st, int posc) {
 
     }
 
-    public Address println() {
+    public void print() {
 
     }
 
