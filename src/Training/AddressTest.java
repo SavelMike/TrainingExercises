0b2b27cab3514b93d994359c48c764ba111f44fc
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-25 22:51:27 +0100

    Programm Address works

--- src/Training/AddressTest.java
+++ src/Training/AddressTest.java
@@ -2,12 +2,11 @@ package Training;
 
 public class AddressTest {
     public static void main(String[] args) {
-        Address add = new Address(34, "Laboratornaya", 2, "Dnepropetrovsk", "Ukraine", 49200);
-        add.print();
-        Address ad2 = new Address(44, "Oosterstraat", "Enschede", "Netherlands", 49199);
-        boolean streetData = add.comesBefore(ad2);
-        System.out.println(streetData);
-
+        Address adr1 = new Address(34, "Laboratornaya", 2, "Dnepropetrovsk", "Ukraine", 49200);
+        adr1.print();
+        Address adr2 = new Address(44, "Oosterstraat", "Enschede", "Netherlands", 49199);
+        boolean comesBeforeResult = adr1.comesBefore(adr2);
+        System.out.println(comesBeforeResult);
     }
 
 }
